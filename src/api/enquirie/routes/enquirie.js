01d0de79cfec2ea033cbb69da6fa1aa69a52ec5b
Håkon Volden <hakon.volden@gmail.com>
2022-12-26 11:46:15 +0100

    added content

---
+++ src/api/enquirie/routes/enquirie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * enquirie router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::enquirie.enquirie');
---
+++ src/api/enquirie/services/enquirie.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * enquirie service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::enquirie.enquirie');
